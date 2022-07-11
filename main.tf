cf322f8804ea03f9252e034beecda905f26a70ea
saprykins <60004824+saprykins@users.noreply.github.com>
2022-07-11 16:19:48 +0200

    Create main.tf
    sample terraform file

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "qwiklabs-gcp-02-f833af48a856"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
